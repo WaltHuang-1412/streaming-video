860a95ba0253f91718cf88337ab91231151e252a
Walter Huang <walt.huang.1412@gmail.com>
2023-05-16 22:11:43 +0800

    update

--- src/views/home/ForYou.vue
+++ src/views/home/ForYou.vue
@@ -5,13 +5,17 @@
   color: white;
   .swiper-slide {
     position: relative;
+    [data-is-hidden='true'] {
+      display: none;
+    }
+
     &__cover {
       width: 100%;
       height: 100%;
       background-size: contain;
       background-repeat: no-repeat;
       background-position: center center;
-      background-color: rgba($color: #000000, $alpha: 0.3);
+      background-color: rgba($color: #000000, $alpha: 1);
     }
     &__title {
       position: absolute;
@@ -22,26 +26,35 @@
       width: 100%;
       height: 100%;
     }
+
+    &__shadow {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      left: 0;
+      right: 0;
+    }
   }
 }
 </style>
 <template lang="pug">
-swiper.for-you(v-if="isArray(data)" direction="vertical")
+swiper.for-you(v-if="isArray(data)" direction="vertical" @slide-change="handleSlideChange")
   template(v-for="(item,index) in data")
     swiper-slide
-      //- .swiper-slide__cover(:style="{backgroundImage: 'url(' + item.cover + ')'}")
-      .swiper-slide__title {{ item.title }}
+      .swiper-slide__cover(:style="{backgroundImage: 'url(' + item.cover + ')'}" :data-is-hidden="selectedVideo.id === index")
+      //- .swiper-slide__title {{ item.title }}
       //- video(:ref="`video${index}`").swiper-slide__video
       //-   source(:src="item.play_url")
-
-      video-player.swiper-slide__video.vjs-big-play-centered( :src="item.play_url"
-                                                              :currentSrc="item.play_url"
-                                                              :poster='item.cover'
-                                                              :autoplay="true")
+      .swiper-slide__shadow(:id="`test${index}`")
+Teleport(v-if="selectedVideo.id >= 0" :to="`#test${selectedVideo.id}`")
+  video-player.swiper-slide__video.vjs-big-play-centered( ref="videoRef"
+                                                          :src="selectedVideo.src"
+                                                          :poster='selectedVideo.cover')
+  
 
 </template>
 <script lang="ts">
-import { defineComponent, ref, Ref, onMounted, shallowRef } from 'vue'
+import { defineComponent, ref, Ref, onMounted, shallowRef, reactive } from 'vue'
 import { getForYouList } from '@/library/axios/api'
 import { ITestResponse } from '@/types'
 import { Swiper, SwiperSlide } from 'swiper/vue'
@@ -58,6 +71,13 @@ export default defineComponent({
       data.value = items
     }
     const player = shallowRef<any>()
+    const selectedVideo = reactive({
+      id: -1,
+      src: '',
+      cover: '',
+      autoPlay: true
+    })
+    const videoRef: Ref<any> = ref(null)
 
     const handleMounted = (payload: any) => {
       player.value = payload.player
@@ -68,14 +88,40 @@ export default defineComponent({
       console.log('Basic player event', log)
     }
 
+    const handleSlideChange = (swiper: any) => {
+      const { activeIndex } = swiper
+      chooseVideo(activeIndex)
+    }
+
+    const chooseVideo = (value: number) => {
+      selectedVideo.id = value
+      if (data.value) {
+        selectedVideo.src = data.value[value].play_url
+        selectedVideo.cover = data.value[value].cover
+      }
+
+      if (videoRef.value) {
+        console.log((videoRef.value))
+      }
+    }
+
     const initialize = async () => {
       await getData()
+      chooseVideo(0)
     }
 
     onMounted(() => {
       initialize()
     })
-    return { data, isArray, handleMounted, handleEvent }
+    return {
+      data,
+      isArray,
+      handleMounted,
+      handleEvent,
+      handleSlideChange,
+      selectedVideo,
+      videoRef
+    }
   }
 })
 </script>
